75928132c4af206b1e0e25bbcf7955097369a508
Vasilpap <vasilpap15702@gmail.com>
2026-02-22 17:14:20 +0200

    fix: disable PKCE for HTTP lab Keycloak init

--- src/main.js
+++ src/main.js
@@ -24,6 +24,7 @@ app.use(VueKeycloak, {
   init: {
     onLoad: 'check-sso',
     checkLoginIframe: false,
+    pkceMethod: false,
     silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
   },
 
